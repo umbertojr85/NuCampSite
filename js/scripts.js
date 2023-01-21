599ad19d8bd39107d8de59973fc21263ae1117b0
umbertojr85 <umbertojr85@gmail.com>
2023-01-21 12:01:55 -0800

    Week 4 Assignment

--- js/scripts.js
+++ js/scripts.js
@@ -11,4 +11,18 @@ $(function () {
             $("#carouselButton").children("i").addClass("fa-pause");
         }
     });
+
+    $("#reserveButton").click(function () {
+        $("#reserveModal").modal("show");
+    })
+    $("#loginButton").click(function () {
+        $("#loginModal").modal("show");
+    })
 });
+/*
+$(function () {
+    $("#reserveButton").click(function () {
+        $("#reserveModal").modal("show");
+    })
+})
+*/
